844e27a15a8ec5725324ed140495013e80ed3c54
chuvakov <4ymail@mail.ru>
2021-11-11 19:33:34 +0300

    Добавьте файлы проекта.

---
+++ Snake/Enams/MoveDirection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake.Enams
+{
+    public enum MoveDirection
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    }
+}
---
+++ Snake/Models/Point.cs
@@ -0,0 +1,53 @@
+﻿using Snake.Enams;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake.Models
+{
+    //TODO: реализовать класс карты (что бы ее так же можно было рисовать с помощью метода draw)
+    public class Point
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public char Symbol { get; set; }
+
+        public Point (int x, int y, char symbol)
+        {
+            X = x;
+            Y = y;
+            Symbol = symbol;
+        }
+
+        public void Move(MoveDirection direction, int count)
+        {
+            switch (direction)
+            {
+                case MoveDirection.Up:
+                    Y -= count;
+                    break;
+
+                case MoveDirection.Right:
+                    X += count;
+                    break;
+
+                case MoveDirection.Down:
+                    Y += count;
+                    break;
+
+                case MoveDirection.Left:
+                    X -= count;
+                    break;  
+            }
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write(Symbol);
+        }
+    }
+}
